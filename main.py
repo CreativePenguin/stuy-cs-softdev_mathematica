25b648641ae1a58125d788111145e7994242a708
Winston Peng <panda501wp@gmail.com>
2020-03-06 12:44:58 -0500

    Save

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from wolframclient.language import wl
 from wolframclient.evaluation import WolframLanguageSession
 
-session = WolframLanguageSession
-print(wl.StringReverse('abc'))
-# print(session.evaluate(wl.StringReverse('abc')))
-print(wl.Now)
+session = WolframLanguageSession()
+session.evaluate(wl.StringReverse('abc'))  # StringReverse["abc"]
+session.evaluate(wl.MinMax([1, 5, -3, 9])  # MinMax[{1, 5, -3, 9}]
